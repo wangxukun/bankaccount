398a40649acfca9d640d8a8eead98cb2361ce33f
wang zilin <1436505310@qq.com>
2015-03-23 14:41:34 +0800

    20150323

--- src/xdl/wxk/financing/dao/PageInfoDAO.java
+++
@@ -1,9 +0,0 @@
-package xdl.wxk.financing.dao;
-
-import java.sql.SQLException;
-
-import xdl.wxk.financing.tools.PageInfo;
-
-public interface PageInfoDAO {
-	PageInfo getOperatorPageInfo(int offset, int rowCount) throws SQLException;
-}
